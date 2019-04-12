a02f49ec7c219983ae8cf9274a058e38f709eebe
felipe.rodrigues <felipe.rodrigues@kurier.com.br>
2019-04-12 14:09:17 -0300

    Fixing relative path on volume

--- example/docker-run.sh
+++ example/docker-run.sh
@@ -2,4 +2,4 @@
 
 # Just a small usage example:
 
-docker run --rm -v ./input:/in -v ./output:/out felipederodrigues/tesseract-captcha-breaker:latest bash -c "tesseract /in/captcha.svl /out/result --psm 10 --oem 3 -c tessedit_char_whitelist=1234567890"
+docker run --rm -v $(pwd)/input:/in -v $(pwd)/output:/out felipederodrigues/tesseract-captcha-breaker:latest bash -c "tesseract /in/captcha.svl /out/result --psm 10 --oem 3 -c tessedit_char_whitelist=1234567890"
